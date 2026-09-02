67da843b7454abd0b757933a7298b8664fc27716
ProctHeath <168716314+ProctHeath@users.noreply.github.com>
2026-09-02 23:09:02 +0200

    Update company name and tagline in config.js

--- src/data/config.js
+++ src/data/config.js
@@ -1,6 +1,6 @@
 const config = {
-  name: 'EnderTechX',
-  tagline: 'The Developer For The Web',
+  name: 'TechXCorp',
+  tagline: 'The Future of Web Coding',
   description: "Création d'applications web, mobiles et IoT avec les technologies modernes.",
   heroSubtitle: 'Fullstack & Mobile Developer',
   heroCommand: 'git clone https://github.com/EnderTechX/portfolio.git',
